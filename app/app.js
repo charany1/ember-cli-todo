0c58ca420f1c51f90c16a111c3177a418446ecee
Damion Nabarrete <damion@damionjn.com>
2014-09-14 12:47:13 -0600

    Removed component from app.js- let resolver do the work

--- app/app.js
+++ app/app.js
@@ -1,10 +1,8 @@
 import Ember from 'ember'
 import Resolver from 'ember/resolver'
 import loadInitializers from 'ember/load-initializers'
-import EditTodo from './components/edit-todo'
 
 Ember.MODEL_FACTORY_INJECTIONS = true
-Ember.Handlebars.helper('edit-todo', EditTodo)
 
 var App = Ember.Application.extend({
   modulePrefix: 'todo', // TODO: loaded via config
